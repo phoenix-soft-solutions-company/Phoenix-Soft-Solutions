fc53d3382e48c7af09a055bc5a276d55dc6c9995
shammisk <ssamankumari6@gmail.com>
2024-08-09 20:20:01 +0530

    edit devops

--- frontend/src/sub-pages/services/DevopsService.jsx
+++ frontend/src/sub-pages/services/DevopsService.jsx
@@ -28,8 +28,8 @@ const DevOps = () => {
           <div className="w-24 h-1 bg-red-500 mx-auto my-4"></div>
           <p className="text-lg lg:text-xl">
             As a long-standing DevOps Services Company, Saigon Technology offers
-            high quality and affordable Devops<br></br> consulting and implementation
-            services for startups and enterprises in the world.
+            high quality and affordable Devops<br></br> consulting and
+            implementation services for startups and enterprises in the world.
           </p>
           <img
             src={service1}
@@ -37,23 +37,69 @@ const DevOps = () => {
             className="mt-8 w-full lg:w-1/2 mx-auto"
           />
         </div>
+      </div>
+      {/* Second Section */}
+      <div className="text-center mb-12">
+        <h1 className="text-3xl lg:text-4xl font-bold uppercase mb-3">
+          DevOps to Revolutionize Your IT Operations
+        </h1>
 
-        {/* Second Section */}
-        <div className="text-center mb-12">
-          <h1 className="text-3xl lg:text-4xl font-bold uppercase mb-3">
-            DevOps to Revolutionize Your IT Operations
+        <p className="text-lg lg:text-xl">
+          You can fully optimize the software development related activities for
+          your business with DevOps.<br></br> <br></br>DevOps (Development &
+          Operations) is a technique that was created to integrate software
+          developers with operation teams so that high-quality<br></br> software
+          could be delivered more quickly. Specifically, the use of tool-chain
+          pipelines, monitoring, collaboration, automation, and <br></br>cloud
+          adoption are all components that are leveraged by a DevOps services
+          company.
+        </p>
+        <div className="w-24 h-1 bg-red-500 mx-auto my-4"></div>
+        {/* New Section */}
+        <div className="text-center mb-12 mt-11 bg-gray-100">
+          <h1 className="text-3xl lg:text-4xl font-bold uppercase ">
+            Taking Advantage of Partnering with Saigon<br></br> Technology for
+            DevOps Services
           </h1>
+          <img
+            src={service1}
+            alt="New Section Image"
+            className="mt-8 w-full lg:w-1/2 mx-auto"
+          />
 
-          <p className="text-lg lg:text-xl">
-            You can fully optimize the software development related activities
-            for your business with DevOps.<br></br> <br></br>DevOps (Development & Operations) is
-            a technique that was created to integrate software developers with
-            operation teams so that high-quality<br></br> software could be delivered
-            more quickly. Specifically, the use of tool-chain pipelines,
-            monitoring, collaboration, automation, and <br></br>cloud adoption are all
-            components that are leveraged by a DevOps services company.
-          </p>
-          <div className="w-24 h-1 bg-red-500 mx-auto my-4"></div>
+          {/* Centering the List with Left Padding */}
+          <div className="flex justify-center mt-8">
+            <ul className="text-lg lg:text-xl space-y-4 text-left list-disc list-inside pl-8 max-w-3xl">
+              <li>
+                <span className="font-bold">
+                  Speed up the rollout of new software:
+                </span>{" "}
+                Integrate and automate supervision, quality assessment, and
+                corrective actions using toolchains of DevOps Services & DevOps
+                solutions.
+              </li>
+              <li>
+                <span className="font-bold">
+                  Generate software of an excellent caliber:
+                </span>{" "}
+                Produce software outputs of the highest possible quality, with
+                prolonged release validation to be performed at each level of
+                the software development life cycle (SDLC) to ensure code
+                quality is always at its best.
+              </li>
+              <li>
+                <span className="font-bold">
+                  Boost the efficiency of developing software:
+                </span>{" "}
+                By automatically detecting errors and eliminating them, the
+                downtime will be minimized as much as possible.
+              </li>
+              <li>
+                <span className="font-bold">Fourth</span> item in the list with
+                a brief description.
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
