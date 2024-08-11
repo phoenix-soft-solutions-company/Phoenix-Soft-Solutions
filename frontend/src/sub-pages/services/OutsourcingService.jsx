43caef8c7b785f356aba5e4c8ce0946a7b79bb87
shammisk <ssamankumari6@gmail.com>
2024-08-11 21:28:54 +0530

    push

--- frontend/src/sub-pages/services/OutsourcingService.jsx
+++ frontend/src/sub-pages/services/OutsourcingService.jsx
@@ -13,7 +13,7 @@ const outsourcing = () => {
         />
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <h1 className="text-2xl lg:text-5xl font-serif font-bold tracking-widest text-white uppercase">
-            DEVOPS SERVICE
+            SOFTWARE DEVELOPMENT OUTSOURCING
           </h1>
         </div>
       </div>
