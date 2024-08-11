8f5005748efb267e1b0df649962643e31562de2c
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-08-11 18:43:39 +0530

    modify map

--- frontend/src/components/CompanyMap.jsx
+++ frontend/src/components/CompanyMap.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Tooltip, useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import "leaflet-curve"; // Import the leaflet-curve plugin
+import "leaflet-curve";
 
 // Define custom icon options
 const redIconOptions = {
@@ -15,27 +15,40 @@ const redIconOptions = {
   shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
 };
 
+const blueIconOptions = {
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+};
+
 const redIcon = L.icon(redIconOptions);
+const blueIcon = L.icon(blueIconOptions);
 
 // Coordinates for the locations
 const locations = [
   { name: "New Zealand", position: [-41.5000831, 172.8344077], icon: redIcon },
   { name: "Sri Lanka", position: [7.5554942, 80.7137847], icon: redIcon },
-  { name: "Australia", position: [-24.7761086, 134.755], icon: redIcon },
-  { name: "United Kingdom", position: [54.7023545, -3.2765753], icon: redIcon },
+  { name: "Australia", position: [-24.7761086, 134.755], icon: blueIcon }, // Blue icon for Australia
   { name: "United Kingdom", position: [54.7023545, -3.2765753], icon: redIcon },
   { name: "Sweden", position: [59.6749712, 14.5208584], icon: redIcon },
 ];
 
 // Function to create a Bézier curve path between two locations
 const createCurvePath = (from, to) => {
+  const controlPoint1 = [(from[0] + to[0]) / 2]; // Midpoint with some offset for curve height
+  const controlPoint2 = [(from[0] + to[0]) / 2]; // Midpoint with some offset for curve height
+
   return [
     "M",
     from, // Move to the starting point
     "C",
-    [from[0]], // Control point 1 (adjust to shape the curve)
-    [to[1]], // Control point 2 (adjust to shape the curve)
-    to, // End at the destination point
+    controlPoint1,
+    controlPoint2,
+    to, // Create curve with control points
   ];
 };
 
@@ -45,7 +58,7 @@ const CurvedPolyline = ({ positions }) => {
   React.useEffect(() => {
     if (L.curve) {
       const path = createCurvePath(positions[0], positions[1]);
-      const curve = L.curve(path, { color: "red", weight: 2, dashArray: "10, 10" }).addTo(map);
+      const curve = L.curve(path, { color: "red", weight: 1 }).addTo(map);
 
       return () => {
         map.removeLayer(curve);
@@ -60,16 +73,16 @@ const CompanyMap = () => {
   const AustraliaPosition = locations.find((location) => location.name === "Australia").position;
 
   return (
-    <div className="w-full h-[500px]">
+    <div className="w-full h-[600px]">
       <MapContainer
-        center={[20, 0]}
+        center={[20, 80]}
         zoom={2}
-        style={{ height: "500px", width: "100%" }}
+        style={{ height: "600px", width: "100%" }}
         className="rounded-lg shadow-lg">
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {locations.map((location, index) => (
           <Marker key={index} position={location.position} icon={location.icon || null}>
-            <Popup>{location.name}</Popup>
+            <Tooltip permanent>{location.name}</Tooltip>
           </Marker>
         ))}
         {locations
