58ea5008bde7c3741a2f0dac2804e51a036477f7
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-08-18 22:26:05 +0530

    coonect backend to counter

--- frontend/src/components/CounterExp.jsx
+++ frontend/src/components/CounterExp.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Counter from "./Counter";
 import icon1 from "../constants/images/icons/calendar.png";
 import icon2 from "../constants/images/icons/project.png";
@@ -6,6 +7,28 @@ import icon3 from "../constants/images/icons/skill.png";
 import icon4 from "../constants/images/icons/client.png";
 
 function CounterExp() {
+  const [counters, setCounters] = useState({
+    experiences: 0,
+    projects: 0,
+    experts: 0,
+    clients: 0,
+  });
+
+  useEffect(() => {
+    const fetchCounters = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_BASE_URL}/counter/66c21af52a7dd94e2cd73e8c`
+        );
+        setCounters(response.data);
+      } catch (error) {
+        console.error("Error fetching counter data:", error);
+      }
+    };
+
+    fetchCounters();
+  }, []);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-5 justify-center w-full p-8 rounded shadow-lg">
       <div className="flex flex-row gap-4">
@@ -15,12 +38,11 @@ function CounterExp() {
         <div className="flex flex-col justify-center h-full">
           <h1 className="text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500">
             <Counter
-              threshold={5}
+              threshold={counters.experiences}
               increment={1}
               className="bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500"
             />
           </h1>
-
           <h2 className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500">
             Years Of Experience
           </h2>
@@ -34,7 +56,7 @@ function CounterExp() {
         <div className="flex flex-col justify-center h-full">
           <h1 className="text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
             <Counter
-              threshold={20}
+              threshold={counters.projects}
               increment={1}
               className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500"
             />
@@ -52,7 +74,7 @@ function CounterExp() {
         <div className="flex flex-col justify-center h-full">
           <h1 className="text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-red-500">
             <Counter
-              threshold={10}
+              threshold={counters.experts}
               increment={1}
               className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-red-500"
             />
@@ -70,7 +92,7 @@ function CounterExp() {
         <div className="flex flex-col justify-center h-full">
           <h1 className="text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-purple-500">
             <Counter
-              threshold={30}
+              threshold={counters.clients}
               increment={1}
               className="bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-purple-500"
             />
