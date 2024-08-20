7cf2d6df0fa881552862c0ed65fde45f69c6cd01
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-08-20 20:30:43 +0530

    add origin

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,15 @@ const handleErrors = require("./middleware/error-handler.middleware");
 
 require("dotenv").config();
 
-app.use(cors());
+const corsOptions = {
+  origin: ["https://tkdeshan.github.io/Xcrop-website/", "http://localhost:3000"],
+  methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
