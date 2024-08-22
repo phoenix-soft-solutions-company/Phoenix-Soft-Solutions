15481ca312b7ee3910687341ee25ca9950410021
shammisk <ssamankumari6@gmail.com>
2024-08-22 16:46:23 +0530

    edit company name

--- frontend/src/pages/Career.jsx
+++ frontend/src/pages/Career.jsx
@@ -110,7 +110,7 @@ const Career = () => {
             {
               title: "Opportunities to learn and share what you know",
               description:
-                "Success at Phoenix Soft Solutions means you'll always be facing new challenges - which means you're learning and empowered to overcome your current challenges. To facilitate learning, we provide coaches and learning tracks through our X Croup Academy program. Our current learning tracks include: Product Discovery, Tech Practices, Agile Facilitation, Product Management and People Leadership. Plus, there are plenty of opportunities to showcase your skills to your VPT and others at X Croup through our weekly CoPX experience discussions, fortnightly lunch presentations and cross-team huddles.",
+                "Success at Phoenix Soft Solutions means you'll always be facing new challenges - which means you're learning and empowered to overcome your current challenges. To facilitate learning, we provide coaches and learning tracks through our Phoenix Soft Solutions Academy program. Our current learning tracks include: Product Discovery, Tech Practices, Agile Facilitation, Product Management and People Leadership. Plus, there are plenty of opportunities to showcase your skills to your VPT and others at Phoenix Soft Solutions through our weekly CoPX experience discussions, fortnightly lunch presentations and cross-team huddles.",
               imgSrc: card1,
               imgAlt: "Learning opportunities",
             },
@@ -118,7 +118,7 @@ const Career = () => {
               title:
                 "Create impactful software and share in the financial upside",
               description:
-                "At Phoenix Soft Solutions, we love building impactful and purposeful software for our customers. Our customers are building SaaS businesses and the awesome thing about SaaS is that when you are creating an impact for good, then scaling the business also scales the impact. X Croup Ventures is our team ESOP, giving everyone who works at X Croup a chance to own shares in our customers. So when our customers win, we not only take pride in knowing we’ve achieved an awesome impact, but we share in the financial upside.",
+                "At Phoenix Soft Solutions, we love building impactful and purposeful software for our customers. Our customers are building SaaS businesses and the awesome thing about SaaS is that when you are creating an impact for good, then scaling the business also scales the impact. Phoenix Soft Solutions Ventures is our team ESOP, giving everyone who works at Phoenix Soft Solutions a chance to own shares in our customers. So when our customers win, we not only take pride in knowing we’ve achieved an awesome impact, but we share in the financial upside.",
               imgSrc: card2,
               imgAlt: "Showcasing skills",
               reverse: true,
@@ -126,7 +126,7 @@ const Career = () => {
             {
               title: "A supportive & encouraging team environment",
               description:
-                "Our Virtual Product Teams are a core feature of Phoenix Soft Solutions. We value collaboration and co-working, and as part of a VPT, you'll have access to, and be supported by other talented people who are passionate about the success of our partners and X Croup.",
+                "Our Virtual Product Teams are a core feature of Phoenix Soft Solutions. We value collaboration and co-working, and as part of a VPT, you'll have access to, and be supported by other talented people who are passionate about the success of our partners and Phoenix Soft Solutions.",
               imgSrc: card3,
               imgAlt: "Supportive environment",
             },
@@ -218,7 +218,7 @@ const Career = () => {
               question:
                 "I want to learn and develop. Will Phoenix Soft Solutions support me?",
               answer:
-                "Yes, Phoenix Soft Solutions is committed to your professional development. We offer various learning opportunities through our X Croup Academy program, including coaching, learning tracks, and regular knowledge-sharing sessions.",
+                "Yes, Phoenix Soft Solutions is committed to your professional development. We offer various learning opportunities through our Phoenix Soft Solutions Academy program, including coaching, learning tracks, and regular knowledge-sharing sessions.",
             },
             {
               question:
